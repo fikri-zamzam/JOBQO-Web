118fa191176a546c1c1af610072f8b68a79c2cbc
Ade Bagus Pratama <55870942+adebagus01@users.noreply.github.com>
2022-03-10 07:00:31 +0700

    ngerapihin doang

--- perpusku/resources/views/petugas.blade.php
+++ perpusku/resources/views/petugas.blade.php
@@ -4,37 +4,37 @@
 @section('container')
 
 {{-- table --}}
-    <table class="table mt-3">
-        <thead>
-            <tr>
-                <th scope="col">No</th>
-                <th scope="col">Nama</th>
-                <th scope="col">Nim</th>
-                <th scope="col">Email</th>
-                <th scope="col">Jurusan</th>
-                <th scope="col">Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">Test</th>
-                <td>1</td>
-                <td>nama</td>
-                <td>Test</td>
-                <td>Test</td>
-                <td>
-                    <div class="row">
-                        <div class="col-2">
-                            <button class="btn btn-warning">Edit</button>
-                        </div>
-                        <div class="col-2">
-                            <button class="btn btn-danger">Hapus</button>
-                        </div>
+<table class="table mt-3">
+    <thead>
+        <tr>
+            <th scope="col">No</th>
+            <th scope="col">Nama</th>
+            <th scope="col">Nim</th>
+            <th scope="col">Email</th>
+            <th scope="col">Jurusan</th>
+            <th scope="col">Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">Test</th>
+            <td>1</td>
+            <td>nama</td>
+            <td>Test</td>
+            <td>Test</td>
+            <td>
+                <div class="row">
+                    <div class="col-2">
+                        <button class="btn btn-warning">Edit</button>
                     </div>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+                    <div class="col-2">
+                        <button class="btn btn-danger">Hapus</button>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
 
-    @endsection
+@endsection
