6cee4c28f8ad16e716953151a73e558dc7d30f58
Ade Bagus Pratama <55870942+adebagus01@users.noreply.github.com>
2022-03-11 10:53:09 +0700

    hapus welcome.blade

--- perpusku/resources/views/style/main.blade.php
+++ perpusku/resources/views/style/main.blade.php
@@ -17,7 +17,7 @@
     {{-- navbar --}}
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#">Perpus.io</a>
+            <a class="navbar-brand" href="#">Perpusku</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
