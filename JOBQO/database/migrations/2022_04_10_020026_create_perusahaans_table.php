e9ac93eea817b4725cbc9464ffdafaeffa4b8e9f
NaufalHisyam18 <naufalhisyam84@gmail.com>
2022-04-10 14:53:58 +0700

    Create 2022_04_10_020026_create_perusahaans_table.php

---
+++ JOBQO/database/migrations/2022_04_10_020026_create_perusahaans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerusahaansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perusahaans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('bidang');
+            $table->integer('no_telp');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perusahaans');
+    }
+}
