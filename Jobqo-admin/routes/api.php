239d7841839388544e2b0ffe0739841aba70f73d
fikri-zamzam <80694693+fikri-zamzam@users.noreply.github.com>
2022-05-17 15:04:10 +0700

    Update api.php

--- Jobqo-admin/routes/api.php
+++ Jobqo-admin/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,5 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::get('test_api',[TestApiController::class],'getall');
