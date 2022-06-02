f36a5c0cdced9feae03501781ca1d7defed86d7f
NaufalHisyam18 <naufalhisyam84@gmail.com>
2022-06-02 12:08:56 +0700

    Update main.blade.php

--- Jobqo-admin/resources/views/layouts/main.blade.php
+++ Jobqo-admin/resources/views/layouts/main.blade.php
@@ -13,27 +13,27 @@
     {{-- awal ubah css menjadi asset --}}
 
     <!-- Bootstrap -->
-    <link href="../../../templates_assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('templates_assets/vendors/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
     <!-- Font Awesome -->
-    <link href="../../../templates_assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="{{ asset('templates_assets/vendors/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
     <!-- NProgress -->
-    <link href="../../../templates_assets/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <link href="{{ asset('templates_assets/vendors/nprogress/nprogress.css') }}" rel="stylesheet">
     <!-- iCheck -->
-    <link href="../../../templates_assets/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <link href="{{ asset('templates_assets/vendors/iCheck/skins/flat/green.css') }}" rel="stylesheet">
 	
     <!-- bootstrap-progressbar -->
-    <link href="../../../templates_assets/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <link href="{{ asset('templates_assets/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet">
     <!-- JQVMap -->
-    <link href="../../../templates_assets/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+    <link href="{{ asset('templates_assets/vendors/jqvmap/dist/jqvmap.min.css') }}" rel="stylesheet"/>
     <!-- bootstrap-daterangepicker -->
-    <link href="../../../templates_assets/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <link href="{{ asset('templates_assets/vendors/bootstrap-daterangepicker/daterangepicker.css') }}" rel="stylesheet">
 
     <!-- Custom Theme Style -->
-    <link href="../../../templates_assets/build/css/custom.min.css" rel="stylesheet">
+    <link href="{{ asset('templates_assets/build/css/custom.min.css') }}" rel="stylesheet">
 
     {{-- Trix css --}}
 
-    <link rel="stylesheet" href="../../../js/trix/trix.css">
+    <link rel="stylesheet" href="{{ asset('js/trix/trix.css') }}">
 
     {{-- akhir ubah css menjadi asset --}}
 
